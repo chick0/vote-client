d0beb830d3cf13a72f7935dd0ddaf9be1efa4937
chick_0 <chick_0@ch1ck.xyz>
2022-07-02 16:35:20 +0900

    dynamic import component

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,8 @@
+import { wrap } from "svelte-spa-router/wrap";
+
 import Home from "./routes/Home.svelte";
 import MyVotes from "./routes/MyVotes.svelte";
-// Vote
 import Create from "./routes/Create.svelte";
-import Vote from "./routes/Vote.svelte";
-import Panel from "./routes/Panel.svelte";
-import Result from "./routes/Result.svelte";
-// Join
 import Join from "./routes/Join.svelte";
 
 // etc
@@ -15,13 +12,18 @@ import NotFound from "./NotFound.svelte";
 export default {
     "/": Home,
     "/my-votes": MyVotes,
-
     "/create": Create,
-    "/vote/:vote_id": Vote,
-    "/panel/:vote_id": Panel,
-    "/result/:vote_id": Result,
-
     "/join/:vote_id/:code": Join,
+
+    "/vote/:vote_id": wrap({
+        asyncComponent: () => import("./routes/Vote.svelte"),
+    }),
+    "/panel/:vote_id": wrap({
+        asyncComponent: () => import("./routes/Panel.svelte"),
+    }),
+    "/result/:vote_id": wrap({
+        asyncComponent: () => import("./routes/Result.svelte"),
+    }),
 
     "*": NotFound,
 };
