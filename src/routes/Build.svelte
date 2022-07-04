1a4287df868fb90dcda6b9530e2635db3a95a9bd
chick_0 <chick_0@ch1ck.xyz>
2022-07-04 10:49:43 +0900

    update build info page

--- src/routes/Build.svelte
+++ src/routes/Build.svelte
@@ -1,10 +1,10 @@
 <script>
     import { HOST } from "../url.js";
 
+    let version = APP_VERSION;
+
     let git_hash = GIT_HASH.slice(0, 7);
     let git_remote = GIT_REMOTE;
-
-    let version = APP_VERSION;
 
     let status = STATUS_URL;
 
@@ -19,6 +19,17 @@
 <section class="section">
     <div class="container">
         <div class="box">
+            <h5 class="title is-5">env</h5>
+            <p><b>MODE</b>: {import.meta.env.MODE}</p>
+            <p><b>SSR</b>: {import.meta.env.SSR}</p>
+        </div>
+
+        <div class="box">
+            <h5 class="title is-5">client version</h5>
+            <p>{version}</p>
+        </div>
+
+        <div class="box">
             <h5 class="title is-5">git hash</h5>
             <p>{git_hash}</p>
         </div>
@@ -27,22 +38,11 @@
             <h5 class="title is-5">git remote</h5>
             <p>{git_remote}</p>
         </div>
-
-        <div class="box">
-            <h5 class="title is-5">client version</h5>
-            <p>{version}</p>
-        </div>
     </div>
 </section>
 
 <section class="section">
     <div class="container">
-        <div class="box">
-            <h5 class="title is-5">env</h5>
-            <p><b>MODE</b>: {import.meta.env.MODE}</p>
-            <p><b>SSR</b>: {import.meta.env.SSR}</p>
-        </div>
-
         <div class="box">
             <h5 class="title is-5">api host</h5>
             <p>{HOST}</p>
